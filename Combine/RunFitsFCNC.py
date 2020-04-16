c015c2a012d3f993e8b49ddf9a623a28b9fd482a
Samuel May <sammay@ucla.edu>
2020-04-16 14:33:59 -0700

    Update script for extracting limits

--- Combine/RunFitsFCNC.py
+++ Combine/RunFitsFCNC.py
@@ -1,43 +1,61 @@
 import sys, os
 import glob
 
+sys.path.append("../Signal/python")
+import parallel_utils
+
 lumi = {'2016':'35.9', '2017':'41.5', '2018':'59.8'}
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--datacard", help = "path to datacard", type=str, default="../Datacard/Datacard_Hct.txt")
+parser.add_argument("--datacard", help = "path to datacard", type=str, default="../Datacard/Datacard_COUPLING.txt")
 parser.add_argument("--tag", help = "tag", type=str, default = "test")
-parser.add_argument("--coupling", help = "fcnc coupling", type=str, default="hct")
+parser.add_argument("--cats", help = "subcategories to also run limits for", type=str, default = "FCNCHadronicTag_0,FCNCHadronicTag_1,FCNCLeptonicTag_0,FCNCLeptonicTag_1")
+parser.add_argument("--couplings", help = "fcnc coupling", type=str, default="Hut,Hct")
 args = parser.parse_args()
 
-datacard = args.datacard.split("/")[-1]
-datacard_dir = args.datacard.replace(datacard, "")
 
-#os.chdir(datacard_dir)
-os.system("cp %s %s" % (args.datacard, "../Plots/FinalResults/"))
+cats = args.cats.split(",")
+couplings = args.couplings.split(",")
+
+for coupling in couplings:
+    os.system("cp %s %s" % (args.datacard.replace("COUPLING", coupling).replace(".txt", "*"), "../Plots/FinalResults/"))
+
+command_list = []
+
+for coupling in couplings:
+    datacard = args.datacard.split("/")[-1].replace("COUPLING", coupling)
+    datacard_dir = args.datacard.replace(datacard, "")
+
+    datacard_ws = "Datacard_%s_FCNC.root" % (args.tag + "_" + coupling.lower())
+    command = "text2workspace.py %s -o %s -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel -m 125 higgsMassRange=122,128" % (datacard, datacard_ws)
+    command += ' --PO "map=.*/fcnc.*:r_fcnc_%s[1,0,2]" ' % coupling.lower()
+    command_list.append(command)
+
+    for cat in cats:
+        datacard = args.datacard.split("/")[-1].replace("COUPLING", coupling).replace(".txt", "_%s.txt" % cat)
+        datacard_ws = "Datacard_%s_%s_FCNC.root" % (args.tag + "_" + coupling.lower(), cat)
+        command = "text2workspace.py %s -o %s -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel -m 125 higgsMassRange=122,128" % (datacard, datacard_ws)
+        command += ' --PO "map=.*/fcnc.*:r_fcnc_%s[1,0,2]" ' % coupling.lower()
+        command_list.append(command)
+
 os.chdir("../Plots/FinalResults/")
+parallel_utils.submit_jobs(command_list, 12)
+command_list = []
 
-#datacard_ws = "Datacard%s_mu_ttH.root" % args.tag
-datacard_ws = "Datacard%s_mu_FCNC.root" % (args.tag + "_" + args.coupling)
+cats_full = cats
+cats_full.append("")
+for coupling in couplings:
+    for cat in cats_full:
+        cat_string = "" if cat == "" else "_" + cat
+        print "cat_string", cat_string
+        command = "combine -M AsymptoticLimits -m 125 --run blind Datacard_%s_%s%s_FCNC.root -n %s" % (args.tag, coupling.lower(), cat_string, "limit" + cat_string)
+        command_statonly = command + " --freezeParameters allConstrainedNuisances"
 
-command = "text2workspace.py %s -o %s -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel -m 125 higgsMassRange=122,128" % (datacard, datacard_ws)
-#command += ' --PO "map=.*/tth.*:r_ttH[1,0,3]" '
-command += ' --PO "map=.*/fcnc.*:r_fcnc_%s[1,0,2]" ' % args.coupling
+        command += " > limits_%s_%s%s.txt" % (args.tag, coupling.lower(), cat_string)
+        command_statonly += " > limits_%s_%s%s_statonly.txt" % (args.tag, coupling.lower(), cat_string)
 
-print command
-os.system(command)
+        command_list.append(command)
+        command_list.append(command_statonly)
 
-os.system("cp %s %s" % (datacard_ws, "../../Combine/"))
-os.chdir("../../Combine")
-
-#command = "python runFits.py --mode 'mu_ttH' --ext %s" % args.tag
-command = "python runFits.py --mode 'limit_FCNC' --ext %s" % args.tag
-
-print command
-#os.system(command)
-
-#command = "python collectFits.py --mode 'mu_ttH' --ext %s" % args.tag
-command = "python collectFits.py --mode 'mu_FCNC' --ext %s" % args.tag
-
-print command
-#os.system(command)
+parallel_utils.submit_jobs(command_list, 12)
